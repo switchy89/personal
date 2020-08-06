cee70d93b8cdc2556102d04f5e31ae563503f837
Switchy <66003740+switchy89@users.noreply.github.com>
2020-08-06 15:54:04 +0200

    Update userdel.sh

--- userdel.sh
+++ userdel.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-echo "Eliminare Utente"
+clear
+tput bold ; printf '%35s%s%-20s\n' "Eliminare un Utente!" ; tput sgr0
 echo ""
-echo "Lista degli utenti:" ; echo "" ;
+tput bold ; echo "Lista degli utenti:" ; tput sgr0 ; echo "" ;
 awk -F : '$3 >= 500 { print $1 }' /etc/passwd | grep -v '^nobody'
 echo ""
-read -p "Nome utente da rimuovere: " user
+tput bold ; read -p "Nome utente da rimuovere: " user ; tput sgr0
 if [[ -z $user ]]
 then
-	echo "" ; echo "hai inserito un utente non valido" ; echo "" ;
+	tput bold ; echo "" ; echo "hai inserito un utente non valido" ; tput sgr0 ; echo "" ;
 	exit 1
 else
 	if [[ `grep -c /$user: /etc/passwd` -ne 0 ]]
@@ -16,18 +17,18 @@ else
 		if [[ `grep -c $user /tmp/rem` -eq 0 ]]
 		then
 			deluser $user > /dev/null
-			echo "" ; echo "L'utente $user è stato rimosso!" ; echo "" ;
+			tput bold ; echo "" ; echo "L'utente $user è stato rimosso!" ; tput sgr0 ; echo "" ;
 			grep -v ^$user[[:space:]] /root/utenti.db > /tmp/ph ; cat /tmp/ph > /root/utenti.db
 			exit 1
 		else
 			echo "" ; echo "Utente connesso. Disconnessione ..." ;
 			pkill -f "$user"
 			deluser $user > /dev/null
-			echo "" ; echo "L'utente $user è stato rimosso con successo!" ; echo "" ;
+			tput bold ; echo "" ; echo "L'utente $user è stato rimosso con successo!" ; tput sgr0 ; echo "" ;
 			grep -v ^$user[[:space:]] /root/utenti.db > /tmp/ph ; cat /tmp/ph > /root/utenti.db
 			exit 1
 		fi
 	else
-		echo "" ; echo "L'utente $user non esiste!" ; echo "" ;
+		tput bold ; echo "" ; echo "L'utente $user non esiste!" ; tput sgr0 ; echo "" ;
 	fi
 fi
